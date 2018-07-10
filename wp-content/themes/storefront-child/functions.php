9999e06022ed14821682af321ade0c53313df7ee
Darwing Medina <darwing1210@gmail.com>
2018-07-10 09:41:42 -0600

    removing archive from titles

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -232,6 +232,18 @@ function cat_opengraph_image() {
     }
 }
 
+function remove_category_prefix_from_archive_title( $title ) {
+	if ( is_category() ) {
+    $title = single_cat_title( '', false );
+  } elseif ( is_tag() ) {
+    $title = single_tag_title( '', false );
+  } elseif ( is_author() ) {
+    $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+  }
+  return $title;
+}
+add_filter( 'get_the_archive_title', 'remove_category_prefix_from_archive_title' );
+
 // API stuff
 add_filter( 'rest_endpoints', function( $endpoints ){
   // Disabling endpoints
