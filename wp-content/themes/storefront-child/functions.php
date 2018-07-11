b93cd0fe961465031fa1324a9574f7e6c2b4552d
Darwing Medina <darwing1210@gmail.com>
2018-07-11 11:30:13 -0600

    Added new domain to CORS

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -13,7 +13,7 @@ add_filter( 'rest_authentication_errors', function(){
 add_action( 'json_api', function( $controller, $method ) {
   // header( "Access-Control-Allow-Origin: *" );
   $http_origin = $_SERVER['HTTP_ORIGIN'];
-  if ($http_origin == "http://localhost:3000" || $http_origin == "https://tienda.peritagua.com") {  
+  if ($http_origin == "http://localhost:3000" || $http_origin == "https://tienda.peritagua.com" || $http_origin == "https://tienda.heyshopper.co") {  
     header( "Access-Control-Allow-Origin: $http_origin" );
 		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
 		header( 'Access-Control-Allow-Credentials: true' );
