221bb4595baeb4efb066ae8b1f42072de15a1f52
Darwing Medina <darwing1210@gmail.com>
2018-07-27 15:09:54 -0600

    Validation at update

--- wp-content/plugins/multisite-rest-api/controllers/multisite-controller.php
+++ wp-content/plugins/multisite-rest-api/controllers/multisite-controller.php
@@ -131,13 +131,21 @@ class MultisiteController extends WP_REST_Controller {
 	 * @param site_name string The sitename used for the site, will become the path or the subdomain
 	 */
 	public function update_site( $id, $title, $site_name, $user_id ) {
-
+        $update_me = $this->get_site_by_id( $id );
+        if ( !is_wp_error( $delete_me ) && $update_me->blog_id == $id ) {
         // TODO: Check if user in site
-        update_blog_option( $id, 'blogname', $title );
-        update_blog_option( $id, 'home', $site_name );
-        update_blog_option( $id, 'siteurl', $site_name );
-        update_blog_details( $id, array( 'path' => $site_name ) );
-        return $this->get_site_by_id( $id );
+            update_blog_option( $id, 'blogname', $title );
+            update_blog_option( $id, 'home', $site_name );
+            update_blog_option( $id, 'siteurl', $site_name );
+            update_blog_details( $id, array( 'path' => $site_name ) );
+            return $update_me;
+        } else {
+            return new WP_Error(
+                'rest_blog_invalid_id', 
+                __( 'Invalid blog ID.' ),
+                array( 'status' => 404 )
+            );
+        }
     }
 
     /*
